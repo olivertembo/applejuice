79717604d9471a8ca7e8a8ca269133e0aa9e6819
olivertembo <tembokoliver3@gmail.com>
2021-11-17 11:42:14 +0300

    Apple Juice 1.2

--- App.tsx
+++ App.tsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {ThemeProvider} from './src/context/ThemeContext';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Text, View, StyleSheet, Image} from 'react-native';
+import defaulLogo from './src/assets/logo.png';
+
+const logos = [
+  {
+    name: 'Apple Juice 1.0',
+    image: require('./src/assets/logo.png'),
+  },
+  {
+    name: 'Apple Juice next',
+    image: require('./src/assets/excited-apple.gif'),
+  },
+];
+
+const Logo = () => {
+  const [logoImage, setLogoImage] = useState(logos[0].image);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let index = logos.findIndex(logo => logo.image === logoImage); //0
+      if (index === 0) {
+        setLogoImage(logos[1].image);
+      } else {
+        setLogoImage(logos[0].image);
+      }
+    }, 5000);
+  }, []);
+
+  return (
+    <View style={styles.logo}>
+      <Image source={logoImage} style={styles.logo} />
+    </View>
+  );
+};
 
 export default function App(): React.ReactElement | null {
   return (
     <ThemeProvider>
       <SafeAreaProvider>
         <View style={styles.container}>
-          <Image
-            source={require('./src/assets/excited-apple.gif')}
-            style={styles.logo}
-          />
-          <Text style={styles.textLogo}>Apple Juice 1.0</Text>
+          <Logo />
+          <Text style={styles.textLogo}>Apple Juice 1.2</Text>
         </View>
       </SafeAreaProvider>
     </ThemeProvider>
