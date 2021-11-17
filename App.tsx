abd0215b5bdb2bab1832e5c53d0a37cfedd8db70
olivertembo <tembokoliver3@gmail.com>
2021-11-17 11:01:28 +0300

    Added center image for Apple Juice update

--- App.tsx
+++ App.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import {ThemeProvider} from './src/context/ThemeContext';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Text, View, StyleSheet, Image} from 'react-native';
-//import Navigation from './src/navigation';
-import logo from './src/assets/excited-apple.gif';
+
 export default function App(): React.ReactElement | null {
   return (
     <ThemeProvider>
       <SafeAreaProvider>
         <View style={styles.container}>
-          <Image source={logo} style={styles.logo} />
+          <Image
+            source={require('./src/assets/excited-apple.gif')}
+            style={styles.logo}
+          />
           <Text>Apple Juice</Text>
         </View>
       </SafeAreaProvider>
